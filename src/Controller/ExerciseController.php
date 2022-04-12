f31f8913885723485054ef83882349fd7f83d3c7
Benjamin <benjaming@outlook.fr>
2022-04-12 15:44:07 +0200

    Avancements
    Affichage du formulaire question/réponses

--- src/Controller/ExerciseController.php
+++ src/Controller/ExerciseController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Question;
+use App\Entity\Media;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,26 +19,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class ExerciseController extends AbstractController
 {
     #[Route('/maker', name: 'maker')]
-    public function exerciseMaker(Request $request, EntityManagerInterface $manager): Response
+    public function exerciseMakerQuestion(Request $request, EntityManagerInterface $manager): Response
     {
         $question = new Question;
-        $startOfQuestionForm = $this->createFormBuilder($question)
+        $form = $this->createFormBuilder($question)
                             ->add('title', TextType::class)
-                            ->add('explanation', TextareaType::class)
-                            ->add('answer', TextType::class)
+                            ->add('explanation', TextareaType::class, ['required' => false])
+                            ->add('content', FileType::class, ['mapped' => false,
+                                                               'required' => false,
+                                                               'attr' => ['accept' => 'image/*',
+                                                                           'multiple' => 'multiple'
+                                                                          ]])
+                            ->add('answer1', TextType::class, ['mapped' => false,])
+                            ->add('answer2', TextType::class, ['mapped' => false,])
+                            ->add('answer3', TextType::class, ['mapped' => false, 'required' => false])
+                            ->add('answer4', TextType::class, ['mapped' => false, 'required' => false])
+                            ->add('answer5', TextType::class, ['mapped' => false, 'required' => false])
+                            ->add('answer6', TextType::class, ['mapped' => false, 'required' => false])
+                            ->add('isTrue1', CheckboxType::class, ['mapped' => false, 'required' => false])
+                            ->add('isTrue2', CheckboxType::class, ['mapped' => false, 'required' => false])
+                            ->add('isTrue3', CheckboxType::class, ['mapped' => false, 'required' => false])
+                            ->add('isTrue4', CheckboxType::class, ['mapped' => false, 'required' => false])
+                            ->add('isTrue5', CheckboxType::class, ['mapped' => false, 'required' => false])
+                            ->add('isTrue6', CheckboxType::class, ['mapped' => false, 'required' => false])
                             ->getForm();
 
-        //$media = new Media;
-        //$mediaForm = $this->createFormBuilder($media)
-        //                    ->
+        $form->handleRequest($request);
 
-        
+        if( $form->isSubmitted() && $form->isValid() ) {
+        }
 
-
-
-        return $this->render('exercise/maker.html.twig', [
-            'startOfQuestionForm' => $startOfQuestionForm->createView(),
+        return $this->renderForm('exercise/maker.html.twig', [
+            'form' => $form,
         ]);
     }
+
+    
 
 }
